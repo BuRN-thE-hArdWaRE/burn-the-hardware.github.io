0153c45957e652e8c30853bf41637778a94c88e5
Jack-Pots <jpp3062005@gmail.com>
2026-04-11 01:37:44 +0530

    Fix Issues with Deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import { mdsvex } from "mdsvex";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { relative, sep } from "node:path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   compilerOptions: {
-    // defaults to rune mode for the project, except for `node_modules`. Can be removed in svelte 6.
     runes: ({ filename }) => {
       const relativePath = relative(import.meta.dirname, filename);
       const pathSegments = relativePath.toLowerCase().split(sep);
